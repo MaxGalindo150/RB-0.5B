add67106631c99cd4afdc9b4997065d1cc48e069
MaxGalindo150 <maximilianogalindo7@gmail.com>
2025-02-12 18:12:30 -0600

    init

--- scripts/train_tiny_zero.sh
+++ scripts/train_tiny_zero.sh
@@ -32,13 +32,13 @@
 python3 -m verl.trainer.main_ppo \
 data.train_files=$DATA_DIR/train.parquet \
 data.val_files=$DATA_DIR/test.parquet \
-data.train_batch_size=32 \
-data.val_batch_size=64 \
+data.train_batch_size=16 \
+data.val_batch_size=32 \
 data.max_prompt_length=256 \
 data.max_response_length=512 \
 actor_rollout_ref.model.path=$BASE_MODEL \
 actor_rollout_ref.actor.optim.lr=1e-6 \
-actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+actor_rollout_ref.actor.ppo_mini_batch_size=64 \
 actor_rollout_ref.actor.ppo_micro_batch_size=4 \
 actor_rollout_ref.rollout.log_prob_micro_batch_size=4 \
 actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
